a4975dcaae0566414323467d81f41c3961cab990
fe-xiaoxin <zxin088@gmail.com>
2023-10-07 17:04:35 +0800

    🎉 feat(app): 更新

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import type { CAC } from "cac";
+import { execCommand, loggerError, printError, printInfo } from "code-genius";
+
+const npmDepCheck = async () => {
+  try {
+    await execCommand("npx", ["depcheck"], {
+      stdio: "inherit",
+    });
+    printInfo("项目依赖检查通过");
+  } catch (error) {
+    printError(`项目依赖存在一些问题`);
+    loggerError(error);
+    process.exit(1);
+  }
+};
+
+const npmDepCheckInstaller = (cli: CAC) => {
+  return {
+    name: "npmDepCheckInstaller",
+    setup: () => {
+      cli
+        .command(
+          "depcheck",
+          "运行 depcheck 检查过时的、不正确的和未使用的依赖项",
+        )
+        .action(async () => await npmDepCheck());
+    },
+  };
+};
+
+export { npmDepCheck, npmDepCheckInstaller };
